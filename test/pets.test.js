e5797133146cbfc03513fb30fcd88d58a2ccb6e3
ckrountree <jmail0207@gmail.com>
2017-11-06 14:37:34 -0800

    write tests and route for pets

--- test/pets.test.js
+++ test/pets.test.js
@@ -2,13 +2,57 @@ const request = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-describe('<Resource Name Here> API', () => {
+describe('Pets API', () => {
+
     
     beforeEach(() => mongoose.connection.dropDatabase());
+    
+    const animals = {
+        name: 'Milo',
+        type: ['cat','dog','bird','fish','snake'],
+        breed: 'Lab',
+        catchPhrase: {
+            type: 'Go, Boy!'
+        }
+    };
 
-    // remove me!
-    it('is a test', () => {
-        assert.isOk(request);
+    it.only('saves a pet with an id', () => {
+        // let savedPet = null;
+        return request.post('/api/pets')
+            .send(animals)
+            .then(res => {
+                const pet = res.body;
+                assert.isOk(pet._id);
+                assert.equal(pet.name, animals.name);
+            });
+    });
+
+    it.skip('gets all pets by name', () => {
+        
+        const pet1 = {
+            name: 'Milo',
+            type: ['dog', 'fish']
+        };
+        
+        const pet2 = {
+            name: 'Swimmer',
+            type: ['dog', 'fish']
+        };
+
+        const petArray = [pet1, pet2].map(pet => {
+            return request.post('/api/pets')
+                .send(pet)
+                .then(res => res.body);
+        });
+        let saved = null;
+        return Promise.all(petArray)
+            .then(_saved => {
+                saved = _saved;
+                return request.get('/api/fpets');
+            })
+            .then(res => {
+                assert.equal(res.body.name, saved.name);  
+            });
     });
 
 });
