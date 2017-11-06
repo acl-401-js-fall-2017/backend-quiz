d20e2dc24079949ba2186a65ba01c141013b9091
mugworks <greenwood.pdx@gmail.com>
2017-11-06 13:39:54 -0800

    create a model

--- test/pets.test.js
+++ test/pets.test.js
@@ -2,13 +2,38 @@ const request = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-describe('<Resource Name Here> API', () => {
+describe('Pet API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
 
-    // remove me!
+    
     it('is a test', () => {
         assert.isOk(request);
     });
 
+});
+
+
+
+const assert = require('chai').assert;
+const Studio = require('../../lib/models/studio');
+
+describe('Studio model', () => {
+    it('check if studio model is good', () => {
+        const studio = new Studio({
+            name: 'Paramount Pictures',
+            address: {
+                city: 'Hollywood',
+                state: 'California',
+                country: 'USA'
+            }
+        });
+        assert.equal(studio.validateSync(), undefined);
+    });
+
+    it('checks required fields', () => {
+        const studio = new Studio({ });
+        const { errors } = studio.validateSync();
+        assert.equal(errors.name.kind, 'required');
+    });
 });
