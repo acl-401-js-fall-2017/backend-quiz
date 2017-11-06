e53ad9d5e50a7fed9d0524fbec2ec928481672b0
David Nishikawa <triptype@gmail.com>
2017-11-06 14:00:26 -0800

    write red test for post two raves

--- test/pets.test.js
+++ test/pets.test.js
@@ -20,18 +20,50 @@ describe('<Resource Name Here> API', () => {
             catchPhrase: 'Put me down and give me food.'
         }
     ];
+    let raves = [
+        {
+            pet: null,
+            comment: 'adorable but barks too loud',
+            email: null
+        },
+        {
+            pet: null,
+            comment: 'Why won\'t he love me!?',
+            email: null
+        }
+    ];
 
-    describe('Pets', () => {
+    describe('POST', () => {
+
+        let savedPets = null;
+        // let savedRaves = null;
+
         it('posts two pets', () => {
 
             const savePets = [
                 request.post('/api/pets').send(pets[0]).then(res => res.body),
                 request.post('/api/pets').send(pets[1]).then(res => res.body)
             ];
-
             return Promise.all(savePets)
                 .then(res => {
+                    savedPets = res;
                     res.forEach(pet => assert.ok(pet._id));
+                });
+        });
+
+        it('posts two raves from a single user: should fail', () => {
+
+            raves[0].pet = savedPets[0]._id;
+            raves[1].pet = savedPets[1]._id;
+            raves[0].email = raves[1].email = 'tri@bro.com';
+
+            const saveRaves = [
+                request.post('/api/raves').send(raves[0]).then(res => res.body),
+                request.post('/api/raves').send(raves[0]).then(res => res.body)
+            ];
+            return Promise.all(saveRaves)
+                .then(res => {
+                    res.forEach(rave => assert.ok(rave._id));
                 });
         });
     });
