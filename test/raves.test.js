ba83d927641963d3c1896b0318a9251109e4cb85
Shane Moyo <smoyo@pdx.edu>
2017-11-06 13:57:16 -0800

    saving 2 test pets before each rave test

--- test/raves.test.js
+++ test/raves.test.js
@@ -5,18 +5,27 @@ const assert = require('chai').assert;
 describe('<Resource Name Here> API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
+    // let savedPet1 = null; 
+    // let savedPet2 = null;
+    
     const testPet1 = {
         name: 'sam',
         type: 'dog', 
         breed: 'good dog',
         catchPhrase: 'I am a good dog.'
     };
-    let testPet2 = {
+    const testPet2 = {
         name: 'jim',
         type: 'cat', 
         breed: 'good cat',
         catchPhrase: 'I am a good cat.'
     };
+    const testPets = [testPet1, testPet2];
+    return Promise.all(testPets.map(pet => {
+        return request.post('/api/pets')
+            .send(pet)
+            .then(res => res.body);
+    }));
 
     it('Should save a pet with an id', () => {
        
