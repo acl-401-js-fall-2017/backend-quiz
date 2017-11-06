8872db63a8111650d0d3760bb6ad533e7d39001f
Shane Moyo <smoyo@pdx.edu>
2017-11-06 13:42:35 -0800

    added a failing save a pet test

--- test/pets.test.js
+++ test/pets.test.js
@@ -5,10 +5,27 @@ const assert = require('chai').assert;
 describe('<Resource Name Here> API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
+    const testPet1 = {
+        name: 'sam',
+        type: 'dog', 
+        breed: 'good dog',
+        catchPhrase: 'I am a good dog.'
+    };
+    // let testPet2 = {
+    //     name: 'jim',
+    //     type: 'cat', 
+    //     breed: 'good cat',
+    //     catchPhrase: 'I am a good cat.'
+    // };
 
     it('Should save a pet with an id', () => {
-        return request.post()
-        assert.isOk(request);
+        return request.post('/api/pets')
+            .send(testPet1)
+            .then(res => res.body)
+            .then(savedPet => {
+                assert.isOk(savedPet._id);
+                assert.deepEqual(savedPet.name, testPet1.name);
+            });
     });
 
 });
