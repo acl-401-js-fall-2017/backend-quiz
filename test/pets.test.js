e6657a07adb013dcda4a5a91bab0412fa028b491
Robin Miller <rmillerpdx@gmail.com>
2017-11-06 14:26:31 -0800

    create raves get all test

--- test/pets.test.js
+++ test/pets.test.js
@@ -73,12 +73,7 @@ describe('Pets API', () => {
     });
 
 
-    // 1. POST two pets, each of a different type
-    // 1. POST two raves from a single user emails to both of the pets from prior step.
-    // 1. POST two raves from a different user email to both of the pets from first step
-    // 1. **Test** that `GET` `/pets` returns both pets (simple array.length and name check for 
-    // each pet okay)
-    // 1. **Test** that `GET` `/pets?type=<one of the pet types>` only returns the one pet of that type
+    
     // 1. **Test** that `GET` `/raves` returns all four raves plus pet name and type
     // 1. **Test** that `GET` `/pets/:id` for one of the pets returns all fields and has the two raves
 
@@ -93,6 +88,13 @@ describe('Pets API', () => {
         return request.get('/api/pets/getPet?type=cat')
             .then( got => {
                 assert.equal(got.body.type, 'cat');
+            });
+    });
+
+    it('should get all raves from db plus pet name and type', () => {
+        return request.get('/api/raves/')
+            .then( got => {
+                assert.equal(got.body.length, 4);
             });
     });
 
