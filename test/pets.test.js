418c985e41702879048cef647a14c6d701f4932b
Zac-L <Zachlichens@gmail.com>
2017-11-06 13:38:49 -0800

    created red test for posting two pets

--- test/pets.test.js
+++ test/pets.test.js
@@ -1,14 +1,35 @@
 const request = require('./request');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').connection;
 const assert = require('chai').assert;
 
-describe('<Resource Name Here> API', () => {
+describe('Pet API', () => {
+    let petData = null;
     
-    beforeEach(() => mongoose.connection.dropDatabase());
+    beforeEach(() => {
+        mongoose.dropDatabase();
+
+        petData = [
+            {
+                name: 'Sam',
+                type: 'fish',
+                breed: 'Salmon',
+                catchPhrase: 'good morning'
+            },
+            {
+                name: 'Timmy',
+                type: 'dog',
+                breed: 'Boston',
+                catchPhrase: 'norma jean'
+            }
+        ];
+        
+    });
 
     // remove me!
-    it('is a test', () => {
-        assert.isOk(request);
+    it('Posts two pets, each different type', () => {
+        return request.post('/api/pets')
+            .send(petData)
+            .then(res => assert.ok(res.body));
     });
 
 });
