8091e66e6e74a3421666672b706ee35d537cc90f
Andrew Bodey <a.s.bodey@gmail.com>
2017-11-06 15:00:01 -0800

    skipping test and change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
 
-connect(process.env.MONGODB_URI || 'mongodb://localhost:27019/backend-quiz');
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/backend-quiz');
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
--- test/db.js
+++ test/db.js
@@ -1,5 +1,5 @@
 const connect = require('../lib/connect');
-const url = 'mongodb://localhost:27019/backend-quiz-test';
+const url = 'mongodb://localhost:27017/backend-quiz-test';
 const mongoose = require('mongoose');
 
 before(() => connect(url));    
