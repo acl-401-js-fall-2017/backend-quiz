803a0fbf06b12e175e07cb5f46edaba55d1b5c6a
David Nishikawa <triptype@gmail.com>
2017-11-06 13:50:36 -0800

    write green test for save two pets

--- test/pets.test.js
+++ test/pets.test.js
@@ -6,9 +6,34 @@ describe('<Resource Name Here> API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
 
-    // remove me!
-    it('is a test', () => {
-        assert.isOk(request);
+    let pets = [
+        {
+            name: 'Milk',
+            type: 'dog',
+            breed: 'Maltese',
+            catchPhrase: 'Play with me!'
+        },
+        {
+            name: 'Oreo',
+            type: 'cat',
+            breed: 'mixed',
+            catchPhrase: 'Put me down and give me food.'
+        }
+    ];
+
+    describe('Pets', () => {
+        it('posts two pets', () => {
+
+            const savePets = [
+                request.post('/api/pets').send(pets[0]).then(res => res.body),
+                request.post('/api/pets').send(pets[1]).then(res => res.body)
+            ];
+
+            return Promise.all(savePets)
+                .then(res => {
+                    res.forEach(pet => assert.ok(pet._id));
+                });
+        });
     });
 
 });
