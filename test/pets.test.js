a9e80f34e2bcf3c2a9ea122e60b7f657eb349efa
Shane Moyo <smoyo@pdx.edu>
2017-11-06 14:48:03 -0800

    added failing get pet by id test

--- test/pets.test.js
+++ test/pets.test.js
@@ -48,12 +48,60 @@ describe('<Resource Name Here> API', () => {
             .then(() => {
                 return request.get('/api/pets')
                     .then(pets => {
-                        console.log('I AM PETS!!', pets);
-                        console.log('I AM PETS!!', pets.body);
                         assert.deepEqual(pets.body.length, 2);
                     });
             });
 
+    });
+
+    it('Should get pet by id', () => {
+        let testSavePet = null; 
+        return request.post('/api/pets')
+            .send(testPet1)
+            .then(res => res.body)
+            .then(savedPet => {
+                testSavePet = savedPet.body;
+                let savedRave1 = {
+                    pet: testSavePet._id,
+                    comments: 'I love this pet!',
+                    email: 'smoyo@me.com'
+                };
+                let savedRave2 = {
+                    pet: testSavePet._id,
+                    comments: 'this is the best test!',
+                    email: 'notsmoyo@me.com'
+                };
+                const raves = [savedRave1, savedRave2];
+                return Promise.all(raves.map(rave => {
+                    return request.post('/api/raves')
+                        .send(rave)
+                        .then(saverave => saverave.body);
+                }))
+                    .then( savedRaveArray => {
+                        const gotPetCheck = {
+                            _id: testSavePet._id,
+                            name: 'sam',
+                            type: 'dog',
+                            breed: 'good dog',
+                            catchPhrase: 'I am a good dog.',
+                            raves: [{
+                                _id: savedRaveArray[0]._id,
+                                pet: testSavePet._id,
+                                comments: 'I love this pet!',
+                                email: 'smoyo@me.com'
+                            }, {
+                                _id: savedRaveArray[0]._id,
+                                pet: testSavePet._id,
+                                comments: 'this is the best test!',
+                                email: 'notsmoyo@me.com'
+                            }]
+                        };
+                        return request.get(`/api/pets/${testSavePet._id}`)
+                            .then(gotPet => {
+                                assert.deepEqual(gotPet, gotPetCheck);
+                            });
+                    });
+            });
     });
 
 });
