5dd61e1e34a44b30e7b70b03c746a230f30604e2
Maryussuperboy <martsyalis@gmail.com>
2017-11-06 14:21:22 -0800

    add rave model and route

--- test/pets.test.js
+++ test/pets.test.js
@@ -2,13 +2,37 @@ const request = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-describe('<Resource Name Here> API', () => {
+describe('Pets API', () => {
     
     beforeEach(() => mongoose.connection.dropDatabase());
 
-    // remove me!
-    it('is a test', () => {
-        assert.isOk(request);
+    let petArray = [];
+
+    const pet1 = {
+        name: 'fluffy',
+        type: 'cat',
+        breed:'some'
+    };
+    const pet2 = {
+        name: 'bug',
+        type: 'dog',
+        breed:'other'
+    };
+
+    beforeEach( () => {
+        return request.post('/api/pets')
+            .send([pet1, pet2])
+            .then( ({body}) =>{
+                petArray = body;
+            });
     });
+
+    it('is a test', () => {
+        assert.isOk(petArray[0].breed);
+        assert.isOk(petArray[1].type);
+        assert.notEqual(petArray[0].type, petArray[1].type);
+    });
+
+    
 
 });
