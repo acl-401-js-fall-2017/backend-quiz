d4f10aa6412d0e368732673f00a6cf3fc20bbc46
Christina Mills <chrimills@gmail.com>
2017-11-06 14:12:51 -0800

    post test running

--- test/pets.test.js
+++ test/pets.test.js
@@ -2,13 +2,37 @@ const request = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-describe('<Resource Name Here> API', () => {
+describe('Pet Raves API', () => {
+    var petDog = null;
     
-    beforeEach(() => mongoose.connection.dropDatabase());
+    beforeEach(() => {
+        mongoose.connection.dropDatabase();
+        
+        petDog =  {
+            name: 'Fido',
+            type: 'dog',
+            breed: 'retriever',
+            catchPhrase: 'woof woof'
+        };
+        
+    });
 
-    // remove me!
-    it('is a test', () => {
-        assert.isOk(request);
+    it('saves a pet', () => {
+
+        // var petCat = {
+        //     name: 'Kitty',
+        //     type: 'cat',
+        //     breed: 'black',
+        //     catchPhase: 'meow meow'
+        // };
+
+        return request.post('/api/pets')
+            .send(petDog)
+            .then(res => {
+                const pet = res.body;
+                assert.equal(pet.name, 'Fido');   
+            });
+
     });
 
 });
